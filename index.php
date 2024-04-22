8b05a03cff8608a4b58260e6e8ed27cb399ce690
Vitalii Goncharov <1c.audit@gmail.com>
2024-04-22 13:07:52 +0300

    switch to submodule, update bootstrap code

--- index.php
+++ index.php
@@ -6,58 +6,38 @@ use Src\Csrf;
 $s_csrf = (new Csrf())->get();
 $sdk_app_id = SDK_APP_ID;
 
-echo "
-    <html>
-      <script type=\"text/javascript\">
+echo "<script type=\"text/javascript\">
         const SDK_CSRF_CODE = '$s_csrf';
         const SDK_APP_ID = '$sdk_app_id';
-      </script>
-      <script src=\"sdk/Core/Js/Php/get_class.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Core/Js/Php/empty.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Core/CoreDate.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Core/CoreUrl.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Deferred/SdkDeferred.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Deferred/SdkPromise.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/AssertException.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Config/ConfigRegionSid.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Config/ConfigAbstractMixin.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/AuthorizationSignature.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/ModelAbstract.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/sha256.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/sha3.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/SyncError.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Core/Passport/Login/Enter/EnterModel.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Core/Passport/Login/Enter/NotepadModel.js\" type=\"text/javascript\"></script>
-      <script src=\"sdk/Core/Request/Api/KeySessionModel.js\" type=\"text/javascript\"></script>
-      
-      <script src=\"/SdkConfigMixin.js\" type=\"text/javascript\"></script>
-      
-      <script type=\"text/javascript\">
-      var notepad = new Core_Passport_Login_Enter_NotepadModel();
-        notepad.get().done(function()
-        {
-          var enter = new Core_Passport_Login_Enter_EnterModel();
-          enter.s_login = 'spa.gromakov@gmail.com';
-          enter.s_notepad = notepad.s_notepad;
-          enter.s_password = notepad.hash('lkchpy91');
-          enter.post().done(function()
-          {
-            console.log('User has been authorized');
-          }).fail(function(o_error)
-          {
-            if(o_error['s_message'])
-            console.log(o_error['s_message']);
-            else
-            console.log('An error has occurred while attempt to sign in.');
-          });
-        }).fail(function(o_error)
-        {
-          if(o_error['s_message'])
-            console.log(o_error['s_message']);
-          else
-            console.log('An error has occurred while attempt to create notepad.');
-        });
-      </script>
-    </html>
-  ";
+      </script>";
+?>
 
+<html>
+
+<!-- SDK -->
+<script src="wl-sdk-js/Wellnessliving/Core/Js/Php/get_class.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Core/Js/Php/empty.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Core/CoreDate.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Core/CoreUrl.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Deferred/SdkDeferred.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Deferred/SdkPromise.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/AssertException.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Config/ConfigRegionSid.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Config/ConfigAbstractMixin.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/AuthorizationSignature.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/ModelAbstract.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/sha256.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/sha3.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/SyncError.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Core/Passport/Login/Enter/EnterModel.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Core/Passport/Login/Enter/NotepadModel.js" type="text/javascript"></script>
+<script src="wl-sdk-js/Wellnessliving/Core/Request/Api/KeySessionModel.js" type="text/javascript"></script>
+
+<!-- SDK config -->
+<script src="/SdkConfigMixin.js" type="text/javascript"></script>
+
+<!-- JS application -->
+<script src="src/app.js" type="text/javascript"></script>
+
+
+</html>
